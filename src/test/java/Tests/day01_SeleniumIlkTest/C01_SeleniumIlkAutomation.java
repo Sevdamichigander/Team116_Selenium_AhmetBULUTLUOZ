86655b3ac5be6973e76b500b53eaabe9731e0ec3
Sevda BAGCI KARATAS <sevdabagcikaratas@gmail.com>
2023-06-12 12:51:42 +0200

    ilk test

---
+++ src/test/java/Tests/day01_SeleniumIlkTest/C01_SeleniumIlkAutomation.java
@@ -0,0 +1,18 @@
+package Tests.day01_SeleniumIlkTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_SeleniumIlkAutomation {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com");
+
+
+    }
+}
+/*
+
+
+ */
